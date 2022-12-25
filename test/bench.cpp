d71b043fecad3ea3e006a8a818301f398456cc7b
flumn <avsergeichev@edu.hse.ru>
2022-12-25 18:07:55 +0300

    Solved problemes with memory, scale. Visualization without cycle

--- test/bench.cpp
+++ test/bench.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <list>
+#include <algorithm>
 
 #include <SDL.h>
 #include <SDL_ttf.h>
@@ -28,12 +29,20 @@ struct parametrs {
     std::string type = "none";
     std::string output = "none";
 
+//    int width = 0;
+//    int length = 0;
+
+    float fp_x= 0;
+    float fp_y = 0;
+    float sp_x = 0;
+    float sp_y = 0;
+
     int koef_x = 0;
     int koef_y = 0;
 
 
-    parametrs(const std::vector<std::string> &inputs, const std::string &type , const std::string &output)
-        : inputs(inputs), type(type), output(output){}
+    parametrs(const std::vector<std::string> &inputs, const std::string &type, const std::string &output)
+            : inputs(inputs), type(type), output(output) {}
 };
 
 struct bench_statistics {
@@ -48,118 +57,105 @@ struct bench_statistics {
     std::vector<parametrs> element;
 };
 
-//int getGate(const std::string &gate) {
-//    return std::stoi(gate.substr(1));
-//}
 
-void addEdge(bench_statistics &stats, const std::vector<std::string> &inputs, const std::string &type = "none", const std::string &output = "none"){
+void addEdge(bench_statistics &stats, const std::vector<std::string> &inputs, const std::string &type = "none",
+             const std::string &output = "none") {
     stats.element.emplace_back(inputs, type, output);
 }
 
-void sortGraph(bench_statistics &stats, std::vector <std::vector<parametrs>> &array){
+void sortGraph(bench_statistics &stats, std::vector<std::vector<parametrs>> &matrix) {
     std::vector<parametrs> temp;
-    std::vector <std::vector<parametrs>> temp_array;
 
 //  запись input'ов в временный массив
-    for (int i = 0; i<stats.element.size(); ++i){
-        if (stats.element[i].inputs[0] == "none"){
-            temp.emplace_back(stats.element[i]);
+    for (auto iter = stats.element.begin(); iter != stats.element.end(); ++iter) {
+        std::string type = iter->type;
+        if (type == "input") {
+            temp.emplace_back(*iter);
         }
     }
-    array.emplace_back(temp);
+    matrix.emplace_back(temp);
     temp.clear();
 
 //  запись всех элементов в временный массив
-    for (int i = 0; i<stats.element.size(); ++i){
-        if (stats.element[i].inputs[0] != "none" && stats.element[i].output != "none"){
-            temp.emplace_back(stats.element[i]);
+    for (auto iter = stats.element.begin(); iter != stats.element.end(); ++iter) {
+        std::string type = iter->type;
+        if (type != "input" && type != "output") {
+            temp.emplace_back(*iter);
         }
     }
+
 //  сортировка элементов и запись в матрицу
-////выделяется лишнаяя область памяти
-    int k = 0, flag = 0;
-    std::vector<parametrs> clear;
-    while(temp.size() != 0){
-        for (int i=0; i<array[k].size(); ++i){
-            for (int j=0; j<temp.size(); ++j){
-                if (temp[j].inputs[0] == array[k][i].output || temp[j].inputs[1] == array[k][i].output){
-                    array.emplace_back();
-                    array[k+1].emplace_back(temp[j]);
-                    temp.erase(temp.begin()+j); j--;
+ int layer_num = 0;
+    while (temp.size() != 0) {
+        matrix.emplace_back();
+        for (auto matrix_it = matrix[layer_num].begin(); matrix_it != matrix[layer_num].end(); ++matrix_it) {
+            for (int j = 0; j < temp.size(); ++j) {
+                auto temp_it = std::find(temp[j].inputs.begin(), temp[j].inputs.end(), matrix_it->output);
+                if (temp_it != temp[j].inputs.end()) {
+                    matrix.back().emplace_back(temp[j]);
+                    temp.erase(temp.begin() + j);
+                    j--;
                 }
             }
         }
-        k++;
+        layer_num++;
     }
     temp.clear();
-
-////  перераспределение памяти матрицы -- не работает
-//    for (int i = 0; i <array.size(); ++i)
-//        if (array.empty() == true){ array[i].clear(); array[i].shrink_to_fit();}
-
 
 //  запись output'ов в временный массив
-    for (int i = 0; i<stats.element.size(); ++i){
-        if (stats.element[i].output == "none"){
-            temp.emplace_back(stats.element[i]);
+    for (auto iter = stats.element.begin(); iter != stats.element.end(); ++iter) {
+        if (iter->output == "none") {
+            temp.emplace_back(*iter);
         }
     }
-    array.emplace_back(temp);
+    matrix.emplace_back(temp);
     temp.clear();
-
-
-////  вывод полученной матрицы
-//    for (int i = 0; i < array.size(); ++i ){
-//        for (int j = 0; j < array[i].size(); ++j ){
-//            for (int k = 0; k < array[i][j].inputs.size(); ++k)
-//                std::cout << array[i][j].inputs[k] << "  ";
-//            std::cout << array[i][j].type << "  " << array[i][j].output << " || ";
-//        }
-//        std::cout << std::endl;
-//    }
-
-
-
-//Временное решение перераспределения памяти
-    int i = 0;
-    while(i != array.size()){
-        if (array[i].empty() == false){
-            temp_array.emplace_back(array[i]);
-        }
-        i++;
-    }
-    array.clear();
-    array.shrink_to_fit();
-
-    for (int i = 0; i < temp_array.size(); ++i){
-        array.emplace_back(temp_array[i]);
-    }
-
 }
 
-int maxStr(std::vector <std::vector<parametrs>> &array){
+int maxStr(std::vector<std::vector<parametrs>> &matrix) {
     int max = 0;
-    for (int i = 0; i < array.size(); ++i){
-        if ( max < array[i].size() ) max = array[i].size();
+    for (auto iter = matrix.begin(); iter != matrix.end(); ++iter) {
+        if (max < iter->size()) max = iter->size();
     }
     return max;
 }
 
-void setCoordinates(std::vector <std::vector<parametrs>> &array){
-    for (int i = 0; i < array.size(); ++i) {
-        if (array[i].empty() == false) {
-            for (int j = 0; j < array.size(); ++j) {
-                array[i][j].koef_x = i;
-                array[i][j].koef_y = j;
+void setKoef(std::vector<std::vector<parametrs>> &matrix) {
+
+    for (int i =0; i<matrix.size(); ++i) {
+        for (int j =0; j<matrix[i].size(); ++j) {
+            matrix[i][j].koef_x = i;
+            matrix[i][j].koef_y = j;
+
+            if (matrix[i][j].type == "input"){
+
+                matrix[i][j].sp_x = 10 + (SCREEN_WIDTH/matrix.size()) * i;
+                matrix[i][j].sp_y = 10 + (SCREEN_HEIGHT / maxStr(matrix)) * j;
+
+            }
+
+            if (matrix[i][j].type != "input"){
+                for (auto el_pred_str = matrix[i-1].begin(); el_pred_str != matrix[i-1].end(); ++el_pred_str){
+
+                    auto iter = std::find(matrix[i][j].inputs.begin(), matrix[i][j].inputs.end(), el_pred_str->output);
+                    if (iter != matrix[i][j].inputs.end()) {
+
+                        matrix[i][j].fp_x = el_pred_str->sp_x + 4 * SCREEN_WIDTH / (6*matrix.size());
+                        matrix[i][j].fp_y = el_pred_str->sp_y + SCREEN_WIDTH / (4*matrix.size());
+
+                        matrix[i][j].sp_x = 10 + (SCREEN_WIDTH/matrix.size()) * i;
+                        matrix[i][j].sp_y = 10 + (SCREEN_HEIGHT / maxStr(matrix)) * j;
+                    }
+                }
             }
         }
     }
 }
 
-void matrix_to_vector (std::vector<parametrs> &vector, std::vector <std::vector<parametrs>> &array){
-    for (int i = 0; i < array.size(); ++i){
-        for (int j = 0; j < array[i].size(); ++j){
-            vector.emplace_back(array[i][j]);
+void matrix_to_vector(std::vector<parametrs> &vector, std::vector<std::vector<parametrs>> &matrix) {
+    for (auto i = matrix.begin(); i != matrix.end(); ++i) {
+        for (auto j = i->begin(); j != i->end(); ++j) {
+            vector.emplace_back(*j);
         }
     }
 }
@@ -226,41 +222,6 @@ public:
 }; /* bench_statistics_reader */
 
 
-//class bench_pretty_printer : public bench_reader
-//{
-//public:
-//    /*! \brief Constructor of the BENCH pretty printer.
-//     *
-//     * \param os Output stream
-//     */
-//    bench_pretty_printer( std::ostream& os = std::cout )
-//            : _os( os )
-//    {
-//    }
-//
-//    virtual void on_input( const std::string& name ) const override
-//    {
-//        _os << fmt::format( "INPUT({0})", name ) << std::endl;
-//    }
-//
-//    virtual void on_output( const std::string& name ) const override
-//    {
-//        _os << fmt::format( "OUTPUT({0})", name ) << std::endl;
-//    }
-//
-//    virtual void on_gate( const std::vector<std::string>& inputs, const std::string& output, const std::string& type ) const override
-//    {
-//        _os << fmt::format( "{0} = {1}({2})", output, type, detail::join( inputs, "," ) ) << std::endl;
-//    }
-//
-//    virtual void on_assign( const std::string& input, const std::string& output ) const override
-//    {
-//        _os << fmt::format( "{0} = {1}", output, input ) << std::endl;
-//    }
-//
-//    std::ostream& _os; /*!< Output stream */
-//}; /* bench_pretty_printer */
-
 static void
 dump_statistics(FILE *f, const bench_statistics &st) {
     fprintf(f, "inputs: %u, outputs: %u, num ddfs: %u, num lines: %u\n",
@@ -306,48 +267,53 @@ main(int argc, char *argv[]) {
     }
 
 //  вывод содержащихся в структуре элементов
-    for(int i = 0; i<stats.element.size(); ++i){
-        for (int j = 0; j<stats.element[i].inputs.size(); ++j){
+    std::cout << "All elements of the file" << std::endl;
+    for (int i = 0; i < stats.element.size(); ++i) {
+        for (int j = 0; j < stats.element[i].inputs.size(); ++j) {
             std::cout << stats.element[i].inputs[j] << " ";
         }
         std::cout << " || " << stats.element[i].type << " || ";
         std::cout << stats.element[i].output << std::endl;
     }
 
-
     std::cout << "----------------------------------------" << std::endl;
 
-    std::vector <std::vector<parametrs>> matrix;
-    std::vector <parametrs> vector_of_elements;
+    std::vector<std::vector<parametrs>> matrix;
+    std::vector<parametrs> vector_of_elements;
 
     sortGraph(stats, matrix);
-    setCoordinates(matrix);
+    setKoef(matrix);
     matrix_to_vector(vector_of_elements, matrix);
 
-    //  вывод полученной матрицы
-    for (int i = 0; i < matrix.size(); ++i ){
-        for (int j = 0; j < matrix[i].size(); ++j ){
-            for (int k = 0; k < matrix[i][j].inputs.size(); ++k)
-                std::cout << matrix[i][j].inputs[k] << "  ";
-            std::cout << matrix[i][j].type << "  " << matrix[i][j].output << " | "  << matrix[i][j].koef_x <<
-            "  " << matrix[i][j].koef_y << " | " ;
-        }
-        std::cout << std::endl;
+    //  вывод полученной matrix
+//    std::cout  << "Sorted matrix" << std::endl;
+//    for (int i = 0; i < matrix.size(); ++i) {
+//        for (int j = 0; j < matrix[i].size(); ++j) {
+//            for (int k = 0; k < matrix[i][j].inputs.size(); ++k)
+//                std::cout << matrix[i][j].inputs[k] << "  ";
+//            std::cout << matrix[i][j].type << "  " << matrix[i][j].output << " | " << matrix[i][j].koef_x <<
+//                      "  " << matrix[i][j].koef_y << " | ";
+//        }
+//        std::cout << std::endl;
+//    }
+
+    std::cout  << "vector of elements" << std::endl;
+    for (auto iter = vector_of_elements.begin(); iter != vector_of_elements.end(); ++iter){
+        for (auto inp = iter->inputs.begin(); inp != iter->inputs.end(); ++inp)
+            std::cout << *inp << " ";
+        std::cout << iter->type << " " << iter->output << "  in(" << iter->fp_x << " " << iter->fp_y <<")  out(" <<
+        iter->sp_x << " " << iter->sp_y << ")" << "  |"  << iter->koef_x << " " << iter->koef_y << "|" << std::endl;
     }
+
 
     SDL_Rect elements[vector_of_elements.size()];
 
-//    SDL_Rect inputs[stats.number_of_inputs];
-//    SDL_Rect outputs[stats.number_of_outputs];
-//    SDL_Rect ddfs[stats.number_of_dffs];
-//    SDL_Rect lines[stats.number_of_lines];
 
     // Create window
     SDL_Window *window = SDL_CreateWindow("SDL2_ttf sample",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT,
-                                          SDL_WINDOW_SHOWN);
+                                          SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
         printf("Window could not be created!\n"
                "SDL_Error: %s\n", SDL_GetError());
@@ -362,64 +328,20 @@ main(int argc, char *argv[]) {
             for (int i = 0; i < vector_of_elements.size(); ++i) {
 
                 // Dimensions of the inputs
-                elements[i].w = SCREEN_WIDTH/matrix.size() - 70;
-                elements[i].h = SCREEN_HEIGHT/maxStr(matrix) - 50;
+                elements[i].w = 4 * SCREEN_WIDTH / (6*matrix.size());
+                elements[i].h = SCREEN_WIDTH / (2*matrix.size());
 
-                // Location of the rect
-                elements[i].x = SCREEN_WIDTH / (2 * matrix.size()) + (SCREEN_WIDTH / matrix.size()) * vector_of_elements[i].koef_x;
-                elements[i].y = SCREEN_HEIGHT/ (3*maxStr(matrix)) + (SCREEN_HEIGHT / maxStr(matrix)) * vector_of_elements[i].koef_y;
+
+                // Location of the rects
+                elements[i].x = 10 + (SCREEN_WIDTH/matrix.size()) * vector_of_elements[i].koef_x;
+                elements[i].y = 10 + (SCREEN_HEIGHT / maxStr(matrix)) * vector_of_elements[i].koef_y;
+
 
 
                 SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
                 SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
 
             }
-
-
-//            for (int i = 0; i < stats.number_of_inputs; ++i) {
-//
-//                // Dimensions of the inputs
-//                inputs[i].w = 60;
-//                inputs[i].h = 40;
-//
-//                // Location of the rect
-//                inputs[i].x = SCREEN_WIDTH/(2*matrix.size());
-//                inputs[i].y = 100*i+10;
-//
-//
-//                SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
-//                SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
-//
-//            }
-//
-//            for (int i = 0; i < stats.number_of_outputs; ++i) {
-//
-//                // Dimensions of the outputs
-//                outputs[i].w = 60;
-//                outputs[i].h = 40;
-//
-//                // Location of the rect
-//                outputs[i].x = SCREEN_WIDTH-3*SCREEN_WIDTH/matrix.size();
-//                outputs[i].y = 10;
-//
-//                SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
-//                SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
-//
-//            }
-//
-//            for (int i = 0; i < stats.number_of_dffs; ++i) {
-//                // Dimensions of the inputs
-//                ddfs[i].w = 60;
-//                ddfs[i].h = 40;
-//                // Location of the rect
-//                ddfs[i].x = SCREEN_WIDTH/2;
-//                ddfs[i].y = SCREEN_HEIGHT/stats.number_of_dffs - ddfs[i].h*stats.number_of_dffs/2 + (ddfs[i].h + 10) * i;
-//
-//
-//                SDL_Color textColor = {0x00, 0x00, 0x00, 0xFF};
-//                SDL_Color textBackgroundColor = {0xFF, 0xFF, 0xFF, 0xFF};
-//
-//            }
 
 
             // Event loop exit flag
@@ -444,18 +366,23 @@ main(int argc, char *argv[]) {
                 SDL_RenderClear(renderer);
 
                 // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
+                SDL_SetRenderDrawColor(renderer, 0x00, 0xAA, 0xFF, 0xFF);
 
                 // Draw filled square
                 for (int i = 0; i < vector_of_elements.size(); ++i) {
                     SDL_RenderFillRect(renderer, &elements[i]);
                 }
-//                for (int i = 0; i < stats.number_of_outputs; ++i) {
-//                    SDL_RenderFillRect(renderer, &outputs[i]);
-//                }
-//                for (int i = 0; i < stats.number_of_dffs; ++i) {
-//                    SDL_RenderFillRect(renderer, &ddfs[i]);
-//                }
+
+                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+                // Draw lines
+                for (int i = stats.number_of_inputs; i < stats.element.size(); ++i) {
+                    SDL_RenderDrawLine(renderer, vector_of_elements[i].fp_x,
+                                                    vector_of_elements[i].fp_y,
+                                                        vector_of_elements[i].sp_x,
+                                                            vector_of_elements[i].sp_y);
+                }
+
+
                 // Update screen
                 SDL_RenderPresent(renderer);
             }
@@ -474,5 +401,5 @@ main(int argc, char *argv[]) {
         SDL_Quit();
 
     }
-        return 0;
+    return 0;
 }
