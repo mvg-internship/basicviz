c2458e54940e945781950eae266879aafd109440
yaksase <fortheplamar@gmail.com>
2023-03-07 21:54:23 +0300

    Example of vizualizer with test.xml format provided.

---
+++ extern/pugixml-1.13/cmake-build-debug/CMakeFiles/pugixml-static.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pugixml-static.dir/src/pugixml.cpp.obj"
+  "pugixml.lib"
+  "pugixml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pugixml-static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
