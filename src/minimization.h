4ba89448d5c035dfe62b328738c4888ef36514aa
Dima191 <koshkin_mitya25@mail.ru>
2023-04-18 15:04:27 +0300

    add minimization.h file

---
+++ src/minimization.h
@@ -0,0 +1,7 @@
+#include "layout.h"
+
+#ifndef LSVIS_MINIMIZATION_HPP
+#define LSVIS_MINIMIZATION_HPP
+void layer_sweep_algorithm(Net &net, std::vector<TreeNode> &nodes);
+
+#endif //LSVIS_MINIMIZATION_HPP
